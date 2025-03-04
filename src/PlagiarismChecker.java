4e92131897b12956259519dce15a0a84da6f9173
sierrashaw <sierra.shaw@menloschool.org>
2025-03-04 13:14:38 -0800

    YAYYYY!

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -16,31 +16,6 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
 
-    // Challenge 2: Finding all the longest substrings TO WRITE
-    public static int longestSharedSubstring(String doc1, String doc2) {
-
-        int doc1Len = doc1.length();
-        int doc2Len = doc2.length();
-
-        int[][] longestTo = new int[doc1Len + 1][doc2Len + 1];
-
-        for (int i = 1; i <= doc1Len; i++) {
-            for (int j = 1; j <= doc2Len; j++) {
-                if (doc1.charAt(i - 1) == doc2.charAt(j - 1)) {
-                    longestTo[i][j] = 1 + longestTo[i - 1][j - 1];
-                }
-                else {
-                    longestTo[i][j] = Math.max(longestTo[i][j - 1], longestTo[i - 1][j]);
-                }
-            }
-        }
-
-        return longestTo[doc1Len][doc2Len];
-
-        // add after whiteboard
-    }
-
-    /*
     // Challenge 1: Finding the longest String
     public static int longestSharedSubstring(String doc1, String doc2) {
 
@@ -49,6 +24,7 @@ public class PlagiarismChecker {
 
         String[][] longestTo = new String[doc1Len + 1][doc2Len + 1];
 
+        // Fill first row and column with an empty string
         for (int i = 0; i <= doc1Len; i++) {
             longestTo[i][0] = "";
         }
@@ -56,8 +32,11 @@ public class PlagiarismChecker {
             longestTo[0][j] = "";
         }
 
+        // For every letter in each word
         for (int i = 1; i <= doc1Len; i++) {
             for (int j = 1; j <= doc2Len; j++) {
+                // If the letters are equal, add the current letter to the string of the diagonal corner
+                // If not, the current string is equal to the string with larger length between the one above and the one to the left
                 if (doc1.charAt(i - 1) == doc2.charAt(j - 1)) {
                     longestTo[i][j] = longestTo[i - 1][j - 1] + doc1.charAt(i - 1);
                 } else {
@@ -72,7 +51,7 @@ public class PlagiarismChecker {
         return longestTo[doc1Len][doc2Len].length();
     }
 
-*/
+
 
 /*
     // Normal pset: Finding the length of the longest substring
@@ -83,8 +62,11 @@ public class PlagiarismChecker {
 
         int[][] longestTo = new int[doc1Len + 1][doc2Len + 1];
 
+        // For every letter in each word
         for (int i = 1; i <= doc1Len; i++) {
             for (int j = 1; j <= doc2Len; j++) {
+                // If the letters are equal, add 1 to the value of the diagonal corner
+                //If not, the current value is equal to the larger value between the one above and the one to the left
                 if (doc1.charAt(i - 1) == doc2.charAt(j - 1)) {
                     longestTo[i][j] = 1 + longestTo[i - 1][j - 1];
                 }
